855446475f2b1bea2f1ee0d59ffd8f833783ec33
Kha Nguyen <khanguyen1000@gmail.com>
2021-02-16 15:28:49 +0700

    update new documemt

---
+++ NodeJs/course-service/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// APP
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send({
+        "courses": ["HTML",
+            "CSS",
+            "DOCKER",
+            "OOP",]
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`*Running on http://${HOST}:${PORT}`);
